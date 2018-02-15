8be7b70495383c9029af4f1245ca606b9e678ecb
Arnaud <arnaud.meneroud@gmail.com>
2018-02-15 15:51:49 +0000

    before push heroku

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,23 @@
 class TweetsController < ApplicationController
+
+		def new
+  	@tweet = Tweet.new
+  end
+
+  def create
+  	@tweet = Tweet.new(tweet_params)
+  	if @tweet.save
+      
+      SendTweet.new("#{@tweet.content}").perform
+      redirect_to root_path 
+  	else render 'new'
+  	end
+  end
+
+  private
+  	def tweet_params
+      params.permit(:content)
+  	end
+
+	
 end
